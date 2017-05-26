76b58d7cc06162a6031fa970954b60db0a9f527e
loostudy <genius840215@163.com>
2017-05-26 22:10:25 +0800

    First commit

---
+++ spec/loostudy-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LoostudySnippetsView from '../lib/loostudy-snippets-view';
+
+describe('LoostudySnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
